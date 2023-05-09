5751659f2914b26fc07cfb4746f96493a254c280
jinglinwei <weijinglin@sjtu.edu.cn>
2023-05-09 22:22:36 +0800

    add test code

--- pkg/idutil/containerwalker/containerwalkertest.go
+++ pkg/idutil/containerwalker/containerwalkertest.go
@@ -1,3 +1,62 @@
 package containerwalker
 
 // test file
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os/exec"
+	"testing"
+
+	"github.com/containerd/containerd"
+	"minik8s.io/pkg/constant"
+)
+
+func TestNewCmdVersion(t *testing.T) {
+	// construct a new pause container
+	client, err := containerd.New(constant.Cri_uri)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// try to create the SandBox
+	// use cmd to build a pause container
+	// run cmd : nerdctl run -d  --name fake_k8s_pod_pause   registry.aliyuncs.com/google_containers/pause:3.9
+	cmd := exec.Command("nerdctl", "run", "-d", "--name", "test", constant.SandBox_Image)
+	fmt.Println("finish the init of cmd")
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+		t.Error(err)
+	}
+
+	// try to get the SandBox by container walker
+	var foundContainer containerd.Container
+	walker := &ContainerWalker{
+		Client: client,
+		OnFound: func(ctx context.Context, found Found) error {
+			if found.MatchCount > 1 {
+				return fmt.Errorf("container networking: multiple containers found with prefix: %s", "test")
+			}
+			foundContainer = found.Container
+			return nil
+		},
+	}
+
+	n, err := walker.Walk(context.Background(), "test")
+	if err != nil {
+		t.Error(err)
+	}
+	if n == 0 {
+		t.Error(err)
+	}
+
+	fmt.Println("pass test")
+	fmt.Println(foundContainer)
+}
