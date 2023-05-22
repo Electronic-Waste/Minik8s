ade79b34386904772a1767106bc17b2038f5581b
sjq <songjunqing@sjtu.edu.cn>
2023-05-22 20:46:45 +0800

    chore(deployment):modify kubectl get

--- cmd/kubectl/app/get.go
+++ cmd/kubectl/app/get.go
@@ -7,18 +7,13 @@ import (
 
 var (
 	getCmd = &cobra.Command{
-		Use:     "get <resources> | (<resource> <resource-name>)",
+		Use:     "get <resources>",
 		Short:   "get a resource from minik8s",
 		Long:    `get a resource from minik8s`,
-		Example: "apply ",
+		Example: "get pod",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("get called")
 			if len(args) == 1 {
 				if err := GetHandler(args[0]); err != nil {
-					fmt.Println(err.Error())
-				}
-			} else {
-				if err := GetHandlerWithName(args[0], args[1]); err != nil {
 					fmt.Println(err.Error())
 				}
 			}
