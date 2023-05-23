0c9cbdee1a49145ce39dc3fd85b46325d185d404
sjq <songjunqing@sjtu.edu.cn>
2023-05-23 15:59:53 +0800

    chore(autoscaler):some modify

--- pkg/util/prometheus/prometheus.go
+++ pkg/util/prometheus/prometheus.go
@@ -21,51 +21,7 @@ func NewPrometheusClient() {
 }
 
 func RunPrometheus(){
-	image_manager,_ := remote_cli.NewRemoteImageService(remote_cli.IdenticalErrorDelay)
-	runtime_manager := remote_cli.NewRemoteImageServiceByImageService(image_manager)
-	ctx := namespaces.WithNamespace(context.Background(), "default")
 
-	res, err := image_manager.PullImage(ctx, "prom/prometheus:latest")
-	if err != nil {
-		fmt.Println("pull image err")
-		panic(err)
-	}
-	fmt.Printf("pull image %s success\n", res)
-
-	// finish port map first and port random assign after that
-	// format : nervctl run image name  port-map path:path netnamespace command arg...
-	//            arg0  arg1 arg2 arg3     arg4    arg5      arg6         arg7  arg...
-	// construct the Container Object
-	Container := core.Container{}
-	Container.Image = "docker.io/library/prom/prometheus:latest"
-	Container.Name = "prometheus"
-	Container.Ports, err = core.ConstructPorts(os.Args[4])
-	if err != nil {
-		panic(err)
-	}
-	Container.Mounts, err = core.ConstructMounts(os.Args[5])
-	if err != nil {
-		panic(err)
-	}
-	NetNameSpace := os.Args[6]
-	if len(os.Args) < 8 {
-
-	} else {
-		Container.Command = append(Container.Command, os.Args[7])
-		for i, arg := range os.Args {
-			if i < 8 {
-				continue
-			}
-			Container.Args = append(Container.Args, arg)
-		}
-	}
-	fmt.Printf("get the cmd is \n %s \n", Container.String())
-	err := runtime_manager.StartContainer(ctx, Container, NetNameSpace)
-	if err != nil {
-		fmt.Println("start a container failed")
-		panic(err)
-	}
-	
 
 	//client, _ := containerd.New(constant.Cli_uri)
 	//defer client.Close()
--- pkg/util/prometheus/prometheus_test.go
+++ pkg/util/prometheus/prometheus_test.go
@@ -3,12 +3,15 @@ package prometheus
 import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"context"
 	"testing"
+	"fmt"
 )
 
 func TestPrometheus(t *testing.T) {
-	// register a new handler for the /metrics endpoint
-	http.Handle("/metrics", promhttp.Handler())
-	// start an http server
-	http.ListenAndServe(":9090", nil)
+	httpClient := &http.Client{}
+	request, err := http.NewRequest("GET", "localhost:9090/api/v1/query?query=node_cpu_seconds_total")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
