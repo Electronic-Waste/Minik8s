5e503ecbbe745cb651db7151c436efcac2d69217
sjq <songjunqing@sjtu.edu.cn>
2023-05-23 17:26:06 +0800

    fea(deployment):kubectl get

--- cmd/kubectl/app/get.go
+++ cmd/kubectl/app/get.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"minik8s.io/pkg/podmanager"
 )
 
 var (
@@ -12,16 +13,28 @@ var (
 		Long:    `get a resource from minik8s`,
 		Example: "get pod",
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 1 {
-				if err := GetHandler(args[0]); err != nil {
-					fmt.Println(err.Error())
-				}
+			if err := GetHandler(args[0]); err != nil {
+				fmt.Println(err.Error())
 			}
 		},
 	}
 )
 
 func GetHandler(resourceKind string) error {
+	fmt.Println("get handler")
+	switch resourceKind{
+	case "pod":
+		fmt.Println("get pods")
+		pods,err := podmanager.GetPods()
+		if err!=nil{
+			return err
+		}
+		output := "NAMESPACE\tNAME\tSTATUS\t\n"
+		for _,p := range pods{
+			output += "default\t\t" + p.Name + "\t" + string(p.Status.Phase) + "\n"
+		}
+		fmt.Println(output)
+	}
 	return nil
 }
 
