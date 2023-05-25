cc204c907a150df20835cefe461c3489db34cea7
Electronic-Waste <2690692950@qq.com>
2023-05-25 19:29:18 +0800

    feat(dns): Add CreateDNS & DelDNS interface.

--- pkg/kubeproxy/dns/dns.go
+++ pkg/kubeproxy/dns/dns.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"net"
 	"fmt"
-
-	"minik8s.io/pkg/kubeproxy/path"
+	"io/ioutil"
+	"strings"
 )
 
 type DNSFuncInterface interface {
@@ -20,30 +20,54 @@ type DNSController struct {
 	HostsFilePath string
 }
 
-func NewDNSController() (*DNSController, error) {
+func NewDNSController(hostFilePath string) (*DNSController, error) {
 	// Check if file exists
-	_, err := os.Stat(path.HostsFile)
+	_, err := os.Stat(hostFilePath)
 	if err != nil {
 		return nil, err
 	}
 	return &DNSController{
-		HostsFilePath : path.HostsFile,
+		HostsFilePath : hostFilePath,
 	}, nil
 }
 
 func (controller *DNSController) CreateDNSRule(targetIP, hostname string) error {
 	// Check whether the targetIP is valid or not
-	if net.ParseIP(targetIP) != nil {
-		return fmt.Errorf("Invalid targetIP: %s", targetIP)
+	if net.ParseIP(targetIP) == nil {
+		return fmt.Errorf("Invalid targetIP: %s\n", targetIP)
 	}
 	// Open file with r/w privilege
-	file, err := os.OpenFile(controller.HostsFilePath, os.O_RDWR, 777)
+	file, err := os.OpenFile(controller.HostsFilePath, os.O_RDWR | os.O_APPEND, 777)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error in CreateDNSRule: %v\n", err)
 	}
+	ipHostPair := fmt.Sprintf("\n%s %s", targetIP, hostname)
+	var bytesWritten int
+	bytesWritten, err = file.WriteString(ipHostPair)
+	if err != nil {
+		return fmt.Errorf("Error in CreateDNSRule: %v\n", err)
+	}
+	fmt.Printf("bytesWritten: %d", bytesWritten)
 	return nil
 }
 
 func (controller *DNSController) DelDNSRule(targetIP, hostname string) error {
-	return nil
+	content, err := ioutil.ReadFile(controller.HostsFilePath)
+	if err != nil {
+		return fmt.Errorf("Error in DelDNSRule: %v\n", err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+	if len(lines) > 0 && lines[len(lines) - 1] == "" {
+		lines = lines[:len(lines) - 1]
+	}
+	targetLine := fmt.Sprintf("%s %s", targetIP, hostname)
+	for i := range lines {
+		if lines[i] == targetLine {
+			lines = append(lines[:i], lines[i + 1:]...)
+			break
+		}
+	}
+	output := strings.Join(lines, "\n")
+	return ioutil.WriteFile(controller.HostsFilePath, []byte(output), 0644)
 }
