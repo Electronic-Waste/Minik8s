84dbbd6980a24568c22259adce05ae52e5f7f393
Electronic-Waste <2690692950@qq.com>
2023-05-25 20:02:41 +0800

    fix(etcd): Fix empty string bug in etcd.GetWithPrefix.

--- pkg/kubeproxy/nginx/nginx.go
+++ pkg/kubeproxy/nginx/nginx.go
@@ -1,2 +1,55 @@
 package nginx
 
+import (
+	"os"
+
+	"minik8s.io/pkg/apis/core"
+)
+
+type NginxFunInterface interface {
+	// InitNginxConf creates initial nginx.conf
+	InitNginxConf() error
+
+	// ApplyNginxServer adds new server block in nginx.conf
+	ApplyNginxServer(hostName string, paths []core.DNSSubpath) error
+
+	// DeleteNginxServer dels a server block in nginx.conf by hostName
+	DelNginxServer(hostName string) error
+
+	// ReloadNginxSerice apply new nginx config into use
+	ReloadNginxService()  error
+	
+}
+
+type NginxController struct {
+	NginxConfPath string
+}
+
+func NewNginxController(nginxConfPath string) (*NginxController, error) {
+	// Check if file exists
+	_, err := os.Stat(nginxConfPath)
+	if err != nil {
+		return nil, err
+	}
+	return &NginxController{
+		NginxConfPath : nginxConfPath,
+	}, nil
+}
+
+func (controller *NginxController) InitNginxConf() error {
+	lines := []string{}
+	lines = append(lines, "worker_processes 1\n")
+	lines = append(lines, "events {\nworker_connections 1024;\n}\n")
+}
+
+func (controller *NginxController) ApplyNginxServer(hostName string, paths []core.DNSSubpath) error {
+
+}
+
+func (controller *NginxController) DelNginxServer(hostName string) error {
+
+}
+
+func (controller *NginxController)ReloadNginxService() error {
+
+}
