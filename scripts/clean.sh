5ff6d3f1ab1909f5b21be025624d983a3c43ca4f
sjq <songjunqing@sjtu.edu.cn>
2023-05-29 19:10:54 +0800

    fix(autoscaler):fix merge

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 echo "clean etcd"
 
-<<<<<<< HEAD
 #/opt/etcd-v3.4.26/etcdctl del --prefix /node
 /opt/etcd-v3.4.26/etcdctl del --prefix /deployment
 /opt/etcd-v3.4.26/etcdctl del --prefix /service
 /opt/etcd-v3.4.26/etcdctl del --prefix /pods
+/opt/etcd-v3.4.26/etcdctl del --prefix /job
 
 nerdctl stop $(nerdctl ps -a)
 nerdctl rm $(nerdctl ps -a)
-=======
-/opt/etcd-v3.4.26/etcdctl del --prefix /node
-<<<<<<< HEAD
-/opt/etcd-v3.4.26/etcdctl del --prefix /deployment_test
-=======
-/opt/etcd-v3.4.26/etcdctl del --prefix /job
->>>>>>> 246bdcbc8103eb3a17c48085310ffc5a23577d09
-/opt/etcd-v3.4.26/etcdctl del --prefix /service
-/opt/etcd-v3.4.26/etcdctl del --prefix /pods
->>>>>>> develop
