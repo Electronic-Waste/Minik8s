0100ffee894a793981f4b7c777037b420d701d11
sjq <songjunqing@sjtu.edu.cn>
2023-05-29 11:12:56 +0800

    chore(autoscaler):some modify

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "clean etcd"
 
-
 #/opt/etcd-v3.4.26/etcdctl del --prefix /node
 /opt/etcd-v3.4.26/etcdctl del --prefix /deployment
 #/opt/etcd-v3.4.26/etcdctl del --prefix /service
